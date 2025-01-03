44180d625f1a7ffa1327c1efcd2639ff2e68e734
bh-lyu <baihan.lyu@gmail.com>
2025-01-03 22:31:22 +0800

    add kong parcellation plotting iwth fsLR very flat surface

--- plot_example.py
+++ plot_example.py
@@ -1,6 +1,7 @@
 import numpy as np
 from plotting import plot_surface_data_fsLR
 
+############## Example one - plot fsLR_32k surface ############## 
 
 # generate a random den-91k scalar data 
 # you can read if from the .dscalar.nii file
@@ -10,4 +11,15 @@ file_output = 'fsLR_32_surf_example.png'
 cmap = 'jet'  # matplotlib compatible colormap
 threshold = 0.3 # not display the value with absvalue < 0.3
 figure_title = 'A random generated data'
-plot_surface_data_fsLR(data, file_output, surf_type='inflated',threshold = threshold, cmap=cmap, title=figure_title)
+# plot_surface_data_fsLR(data, file_output, surf_type='inflated',threshold = threshold, cmap=cmap, title=figure_title)
+
+
+
+############## Example two - plot kong400 parcellation ############## 
+
+from plotting import plot_Kong_parcellation
+
+data = np.random.uniform(low=-3, high=3, size=400)
+cmap = 'nipy_spectral'
+file_output = 'Kong400_random.png'
+plot_Kong_parcellation(data, file_output, cmap= cmap, title='Kong400_random', title_position=200)
--- plotting.py
+++ plotting.py
@@ -1,78 +1,24 @@
+import os
+from os import system
+import os.path as op
+import numpy as np
 import itertools
 import matplotlib as mpl
+from matplotlib import font_manager
 import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
+from wbplot_images import write_parcellated_image # from wb plot images
 import nibabel as nib
 from nilearn.plotting.surf_plotting import plot_surf_stat_map
 from nilearn.plotting.img_plotting import _get_colorbar_and_data_ranges
 from nilearn.plotting.surf_plotting import _get_ticks_matplotlib, _get_cmap_matplotlib
-import numpy as np
-from cifti_util import *
-from cifti_util import _fetch_fsLR_32k_surf
+from utils.cifti_util import *
+from utils.cifti_util import _fetch_fsLR_32k_surf
+from utils.plot_util import check_vrange
+from zipfile import ZipFile
+from PIL import Image, ImageDraw, ImageFont
 
 
-def extra_surface_data_fsLR(cifti, length = 'full'):
-    """
-    get surface data from dlabel.nii or dscalar.nii
-    the data within the cifti must have length of 91282
-    vertices_num_L = 29696
-    vertices_num_R = 29716
-    vertices_num_32k = 32492
-    :param vertices_32: int; shape: 32492
-    :param hemi: hemisphere: L or R
-
-
-    :return: 
-        if length == 'part', return (data_lh, data_rh) with shape of (29696,) and (29716,)
-        if length == 'full' (default), return (data_lh, data_rh) with both shape (32492,) by filling zeros 
-    """
-
-    template = nib.load(Greyordinates_91282)
-    if isinstance(cifti, np.ndarray):
-        data = cifti
-    elif isinstance(cifti, str):
-        if  (cifti.endswith('.dlabel.nii') or cifti.endswith('dscalar.nii')):
-            cii = nib.load(cifti)
-            data = cii.get_fdata()
-    elif isinstance(cifti, nib.Cifti2Image):
-        cii = cifti
-        data = cii.get_fdata()
-    data = data.ravel()
-    
-    # check the data shape
-    if data.shape[0] == 64984:
-        return(data[:32492], data[32492:])
-    if data.shape[0] == 59412:
-        data = np.pad(data, (0, 91282-59412), constant_values=0)
-    if data.shape[0] != 91282:
-        raise ValueError('the fsLR cifti data must be in 91k .dlabel.nii or ndarray with (91282,) shape')    
-    
-    for brain_model in template.header.get_index_map(1).brain_models:
-
-        if brain_model.brain_structure == 'CIFTI_STRUCTURE_CORTEX_LEFT':
-            cortex_lh = brain_model
-
-        if brain_model.brain_structure == 'CIFTI_STRUCTURE_CORTEX_RIGHT':
-            cortex_rh = brain_model
-         
-    # get the data for each hemisphere, without zeros
-   
-    index_lh = range(cortex_lh.index_offset, cortex_lh.index_offset+cortex_lh.index_count)
-    index_rh = range(cortex_rh.index_offset, cortex_rh.index_offset+cortex_rh.index_count)
-    data_lh_pure, data_rh_pure = data[index_lh], data[index_rh] 
-
-    # get the data onto the 32k space
-    if length == 'full':
-        data_lh = np.zeros(32492)
-        data_rh = np.zeros(32492)
-        vertex_indices_lh = list(cortex_lh.vertex_indices)
-        vertex_indices_rh = list(cortex_rh.vertex_indices)
-        data_lh[vertex_indices_lh] = data_lh_pure
-        data_rh[vertex_indices_rh] = data_rh_pure
-    elif length== 'part':
-        data_lh, data_rh = data_lh_pure, data_rh_pure
-    
-    return (data_lh, data_rh)
 
 
 def plot_surface_data_fsLR(data, file_output=None, surf_type='inflated', title=None, colorbar=True, vrange=None, 
@@ -86,8 +32,9 @@ def plot_surface_data_fsLR(data, file_output=None, surf_type='inflated', title=N
 
     This only support data in the following format
     :data: (1) .dlabel.nii cifti data with 91k density
-            (2) tuple containing (data_lh, data_rh) with 
+           (2) tuple containing (data_lh, data_rh) with 
             data_lh.shape = (29696, ) data_rh.shape=(29716,)
+           (3) numpyarray, in shape (91282,) for fsLR_32k_surf
     
     """
     # set up the default figure
@@ -176,5 +123,237 @@ def plot_surface_data_fsLR(data, file_output=None, surf_type='inflated', title=N
         fig.savefig(file_output)
 
         print(f'figure saved at {file_output}')
+        
+
+def plot_Kong_parcellation(data, figfile,  scene=1, 
+                           figsize=(2400,2400), cmap= 'nipy_spectral', colorbar=True, 
+                           save_cifti=False, vrange = None, title=None, title_position=180):
+    import uuid
+    """
+    :param data: is 400 values of the network, shape is (400,1)
+    :param figfile: the output figure filename ending with '.png'
+    :scene: 1=with parcel outline, 2=without parcel outline
+    :return:
+    """
+    
+
+    if type(data) is np.ndarray:
+        pscalar = data
+    elif isinstance(data, str):
+        d = nib.load(data).get_fdata()
+        pscalar = d[0]
+    else:
+        raise ValueError('data must be a data array or a cifti file ')
+    # The original scene file you created 
+    scene_zip_file=op.join(path_scene,'scene_Kong_dlabel.zip')
+    filename_scene = 'Schaefer_417_dlabel.scene'
+    temp_name = 'ImageParcellated.dlabel.nii'
+
+    try:
+        #generate temporary working folder in output figure directory
+        fig_id = uuid.uuid1().hex
+        fig_dir = os.path.dirname(figfile)
+        temp_dir = op.join(fig_dir,'temp_scene_'+fig_id)
+        os.mkdir(temp_dir)
+        
+        # copy the scene file & SchaeferParcellations directory to the
+        # temp directory as well
+        with ZipFile(scene_zip_file, "r") as z:  # unzip to temp dir
+            z.extractall(temp_dir)
+        scene_file = op.join(temp_dir, 'scene_Kong_dlabel', filename_scene)
+        if not op.isfile(scene_file):
+            raise RuntimeError(
+                "scene file was not successfully copied to {}".format(scene_file))
+
+        # Write `pscalars` to the neuroimaging file which is pre-loaded into the
+        # scene file, and update the colors for each parcel using the file metadata
+        temp_cifti1 = op.join(temp_dir, temp_name)
+        temp_cifti2 = op.join(temp_dir,'scene_Kong_dlabel',temp_name)
+        write_parcellated_image(data=pscalar, fout=temp_cifti1,  cmap=cmap, vrange = vrange)
+
+        #overwrite the original files
+        cmd='cp -f %s %s'%(temp_cifti1, temp_cifti2)
+        system(cmd)
+        
+        if save_cifti:
+            cifti_file = figfile[:-4]+'.dlabel.nii'
+            cmd='cp -f %s %s'%(temp_cifti1, cifti_file)
+            system(cmd)
+        
+        # copy scene file
+        width, height = figsize   # height = 835
+        cmd = path_wb + ' -show-scene "{}" {} "{}" {} {}'.format(scene_file, scene, figfile, width, height)
+        system(cmd)
+        
+        # remove the copied scene folder
+        cmd = 'rm -rf %s'%(temp_dir)
+        system(cmd)
+
+        # add title and colorbar on ImageFile
+        if colorbar:
+            add_colorbar_png(pscalar, figfile, cmap, vrange, orientation='horizontal')
+        if not title is None:
+            add_title_png(figfile, title, title_position=title_position)
+    except Exception as e:
+        # remove the copied scene folder
+        cmd = 'rm -rf %s'%(temp_dir)
+        system(cmd)
+        print(f'An error occured : {e}')
 
 
+
+def extra_surface_data_fsLR(cifti, length = 'full'):
+    """
+    get surface data from dlabel.nii or dscalar.nii
+    the data within the cifti must have length of 91282
+    vertices_num_L = 29696
+    vertices_num_R = 29716
+    vertices_num_32k = 32492
+    :param vertices_32: int; shape: 32492
+    :param hemi: hemisphere: L or R
+
+
+    :return: 
+        if length == 'part', return (data_lh, data_rh) with shape of (29696,) and (29716,)
+        if length == 'full' (default), return (data_lh, data_rh) with both shape (32492,) by filling zeros 
+    """
+
+    template = nib.load(Greyordinates_91282)
+    if isinstance(cifti, np.ndarray):
+        data = cifti
+    elif isinstance(cifti, str):
+        if  (cifti.endswith('.dlabel.nii') or cifti.endswith('dscalar.nii')):
+            cii = nib.load(cifti)
+            data = cii.get_fdata()
+    elif isinstance(cifti, nib.Cifti2Image):
+        cii = cifti
+        data = cii.get_fdata()
+    data = data.ravel()
+    
+    # check the data shape
+    if data.shape[0] == 64984:
+        return(data[:32492], data[32492:])
+    if data.shape[0] == 59412:
+        data = np.pad(data, (0, 91282-59412), constant_values=0)
+    if data.shape[0] != 91282:
+        raise ValueError('the fsLR cifti data must be in 91k .dlabel.nii or ndarray with (91282,) shape')    
+    
+    for brain_model in template.header.get_index_map(1).brain_models:
+
+        if brain_model.brain_structure == 'CIFTI_STRUCTURE_CORTEX_LEFT':
+            cortex_lh = brain_model
+
+        if brain_model.brain_structure == 'CIFTI_STRUCTURE_CORTEX_RIGHT':
+            cortex_rh = brain_model
+         
+    # get the data for each hemisphere, without zeros
+   
+    index_lh = range(cortex_lh.index_offset, cortex_lh.index_offset+cortex_lh.index_count)
+    index_rh = range(cortex_rh.index_offset, cortex_rh.index_offset+cortex_rh.index_count)
+    data_lh_pure, data_rh_pure = data[index_lh], data[index_rh] 
+
+    # get the data onto the 32k space
+    if length == 'full':
+        data_lh = np.zeros(32492)
+        data_rh = np.zeros(32492)
+        vertex_indices_lh = list(cortex_lh.vertex_indices)
+        vertex_indices_rh = list(cortex_rh.vertex_indices)
+        data_lh[vertex_indices_lh] = data_lh_pure
+        data_rh[vertex_indices_rh] = data_rh_pure
+    elif length== 'part':
+        data_lh, data_rh = data_lh_pure, data_rh_pure
+    
+    return (data_lh, data_rh)
+
+
+def add_title_png(img_file,title, fontname = 'Arial', fontsize=100, title_position=300):
+    """
+    Add the title of the image: the contrast
+
+    Parameters
+    ----------
+    img_file : str absolute path to a PNG image file_hctsa
+    title: the title being shown on the figure
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    This function overwrites the existing file_hctsa.
+
+    """
+    img = Image.open(img_file)
+    default_font = font_manager.findfont(fontname)
+    titlefont = ImageFont.truetype(default_font, size=fontsize)
+    image_editable = ImageDraw.Draw(img)
+    image_editable.text((title_position, 15), title, (0,0,0), font=titlefont)
+    img.save(img_file, "PNG", dpi=(300.0, 300.0))
+
+# add color bar11
+def add_colorbar_png(data, img_file, cmap, vrange = None , orientation='horizontal'):
+    """
+    add a color bar for the image_file
+    :param img_file:
+    :param vmin: the minimum value for the color bar
+    :param vmax: the maximum value for the color bar
+    :return: None
+    This function overwrites the existing file_hctsa.
+    """
+    import uuid
+    
+    # open the original figure
+    img = Image.open(img_file) 
+    img_x,img_y = img.size
+    
+    # draw a colorbar and open it
+    colorbar_id = uuid.uuid1().hex
+    fig_dir = os.path.dirname(img_file)
+    file_colorbar = op.join(fig_dir, 'colorbar_%s.png'%(colorbar_id))
+    draw_colorbar(data, figsize=(6, 0.8), file_colorbar=file_colorbar, cmap=cmap, vrange=vrange, orientation=orientation)
+    
+    img_colorbar = Image.open(file_colorbar)
+    img_colorbar_x, img_colorbar_y = img_colorbar.size
+
+    # add the color bar to the original figure
+    img.paste(img_colorbar,(np.int16(img_x/2-img_colorbar_x/2), np.int16(img_y/2-img_colorbar_y/2)) )
+
+    # save it
+    img.save(img_file, dpi=(300.0, 300.0))
+
+    os.system('rm -v %s'%(file_colorbar))
+
+def draw_colorbar(data, figsize, file_colorbar, cmap, fontsize=32, vrange=None, orientation='horizontal'):
+    
+
+    if vrange is None:
+        vrange=(np.min(data), np.max(data)) 
+    else:
+        check_vrange(vrange)
+    
+    vmin=np.int16(np.floor(vrange[0]))
+    vmax=np.round(vrange[1],1)
+    if vmin>=0:
+        
+        vmid=(vmin+vmax)/2
+        if vmax <=6:
+            vmid=np.round(vmid,2)
+        else:
+            vmid=np.round(vmid,1)
+    else:
+        vmid=0
+
+
+    ticks = [vmin, vmid, vmax]
+    #draw a custom colorbar and save a temporary picture
+    fig = plt.figure(constrained_layout=True,figsize=figsize)
+    #plt.rcParams.update({'font.sans-serif':'Arial'})
+    ax = fig.subplots(1,1)
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax, clip=True)
+    fcb = mpl.colorbar.ColorbarBase(norm=norm, cmap=mpl.cm.get_cmap(cmap), orientation=orientation,ax=ax ,ticks = ticks )
+    fcb.ax.set_xticklabels(ticks, fontsize=fontsize)
+
+    
+    #save
+    fig.savefig(file_colorbar, dpi=300,transparent=True)
+    plt.close()
