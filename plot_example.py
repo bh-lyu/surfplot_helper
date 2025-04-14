8245c2d142f95dd12964fd25f27ed643463661df
bh-lyu <baihan.lyu@gmail.com>
2025-04-14 11:07:06 +0800

    adjusting the fsLR plotting function :wq

--- plot_example.py
+++ plot_example.py
@@ -12,23 +12,15 @@ cmap = 'jet'  # matplotlib compatible colormap
 threshold = 0.3 # not display the value with absvalue < 0.3
 figure_title = 'A random generated data'
 
-t1 = time()
-plot_surface_data_fsLR(data, file_output, surf_type='inflated',threshold = threshold, cmap=cmap, title=figure_title, orientation='landscape')
-t2 = time()
-duration = t2-t1
-print(f'original data caused {duration:.2f} seconds ')
-
-t1 = time()
 plot_surface_data_fsLR(data, file_output, surf_type='inflated',threshold = threshold, cmap=cmap, title=figure_title, orientation='square')
-t2 = time()
-duration = t2-t1
-print(f'threaded data caused {duration:.2f} seconds ')
+
+
 
 ############## Example two - plot kong400 parcellation ############## 
 
-# from surfplot_helper.plotting import plot_Kong_parcellation
-#
-# data = np.random.uniform(low=-3, high=3, size=400)
-# cmap = 'nipy_spectral'
-# file_output = 'Kong400_random.png'
-# plot_Kong_parcellation(data, file_output, surf_type='inflated', cmap= cmap, title='Kong400_random', title_position=200)
+from surfplot_helper.plotting import plot_Kong_parcellation
+
+data = np.random.uniform(low=-3, high=3, size=400)
+cmap = 'nipy_spectral'
+file_output = 'Kong400_random.png'
+plot_Kong_parcellation(data, file_output, surf_type='inflated', cmap= cmap, title='Kong400_random', title_position=200)
